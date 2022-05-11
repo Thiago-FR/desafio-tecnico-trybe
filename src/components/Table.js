1b8e79b9b11c528063439d9f5d2b9da7ce8469ac
Thiago <thiago_ssfr@hotmail.com>
2022-05-11 10:33:28 -0300

    feat: Create Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 function Table() {
+  const mockData = [{
+    id: 1,
+    task: 'front-end',
+    date: new Date().getDate(),
+    status: 'Pendente',
+    responsible: 'Thiago'
+  },
+  {
+    id: 2,
+    task: 'front-end',
+    date: new Date().getDate(),
+    status: 'Pendente',
+    responsible: 'Thiago'
+  }]
   return (
     <table className="table">
       <thead>
@@ -10,10 +24,45 @@ function Table() {
           <th scope="col">Data</th>
           <th scope="col">Status</th>
           <th scope="col">Responsável</th>
+          <th scope="col">Editar</th>
         </tr>
       </thead>
+      <tbody>
+          { mockData.length !== 0 && (
+              mockData.map((row) => (
+                <tr key={ `${row.id}-${row.task}` }>
+                  <th scope="row">{ row.id }</th>
+                  <td>{ row.task }</td>
+                  <td>{ row.date }</td>
+                  <td>{ row.status }</td>
+                  <td>{ row.responsible }</td>
+                  <td>
+                    <button type="button">Editar</button>
+                    <button type="button">X</button>
+                  </td>
+                </tr>
+              ))
+          )}
+      </tbody>
     </table>
   );
 }
 
 export default Table;
+
+// { data && data.length !== 0 && (
+//   data.map((row) => (
+//     <tr key={ `${row.id}-${row.task}` }>
+//       <th scope="row">{ row.id }</th>
+//       <td>{ row.task }</td>
+//       <td>{ row.date }</td>
+//       <td>{ row.status }</td>
+//       <td>{ row.responsible }</td>
+//       <td>
+//         <button type="button">Editar</button>
+//         <button type="button">X</button>
+//       </td>
+//     </tr>
+//   ))
+// )}
+
