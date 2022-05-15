9bc3382afc644ba3e9738d1ec5a385c60e289d68
Thiago <thiago_ssfr@hotmail.com>
2022-05-15 19:07:28 -0300

    delete: fetchMocks

--- front-end/src/services/fetchApi.js
+++ front-end/src/services/fetchApi.js
@@ -1,34 +1,3 @@
-const mockData = [
-  {
-    id: 1,
-    task: 'front-end',
-    date: "2022-05-10",
-    status: 'Pendente',
-    responsible: 'Thiago'
-  },
-  {
-    id: 2,
-    task: 'back-end',
-    date: "2022-05-01",
-    status: 'Pendente',
-    responsible: 'Thiago'
-  }]
-
-export function fetchApiMock(setData) {
-  setData([...mockData]);
-}
-
-export function fetchApiMockAdd({ task, responsible }) {
-  const newData = {
-    id: mockData.length,
-    task,
-    date: "11",
-    status: 'Pendente',
-    responsible,
-  }
-  mockData.push(newData);
-}
-
 const URL = 'http://localhost:3001/api/todo-list';
 
 export async function fetchFindAll(setData) {
@@ -36,7 +5,8 @@ export async function fetchFindAll(setData) {
     .then((data) => data.json())
     .then((data) => {
       setData([...data])
-    });
+    })
+    .catch((err) => console.log(err.message));
 };
 
 export async function fetchCreate(data) {
@@ -44,7 +14,8 @@ export async function fetchCreate(data) {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  });
+  })
+  .catch((err) => console.log(err.message));;
 };
 
 export async function fetchUpdate(data, id) {
@@ -52,13 +23,14 @@ export async function fetchUpdate(data, id) {
     method: 'PUT',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  });
+  })
+  .catch((err) => console.log(err.message));;
 };
 
 export async function fetchRemove(id) {
-  console.log(id);
   await fetch(`${URL}/${id}`, {
     method: 'DELETE',
     headers: { "Content-Type": "application/json" },
-  });
+  })
+  .catch((err) => console.log(err.message));;
 };
