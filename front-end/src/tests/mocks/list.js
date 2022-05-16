c501b0b5a10b43f2912ca1e762f88c86f981479b
Thiago <thiago_ssfr@hotmail.com>
2022-05-16 16:55:25 -0300

    test: Thead

--- front-end/src/tests/mocks/list.js
+++ front-end/src/tests/mocks/list.js
@@ -42,3 +42,51 @@ export const listAll = [
     "responsible": "testando2"
   }
 ]
+
+export const lisDesOrder = [
+  {
+    "id": 3,
+    "task": "test3",
+    "date": "2022-05-13T18:09:08.859Z",
+    "status": "Pendente",
+    "responsible": "testando3"
+  },
+  {
+    "id": 2,
+    "task": "test2",
+    "date": "2022-05-13T18:09:08.859Z",
+    "status": "Pendente",
+    "responsible": "testando2"
+  },
+  {
+    "id": 1,
+    "task": "test2",
+    "date": "2022-05-13T18:09:08.859Z",
+    "status": "Pendente",
+    "responsible": "testando1"
+  }
+]
+
+export const listOrderId = [
+  {
+    "id": 1,
+    "task": "test",
+    "date": "2022-05-13T18:09:08.859Z",
+    "status": "Pendente",
+    "responsible": "testando1"
+  },
+  {
+    "id": 2,
+    "task": "test2",
+    "date": "2022-05-13T18:09:08.859Z",
+    "status": "Pendente",
+    "responsible": "testando2"
+  },
+  {
+    "id": 3,
+    "task": "test3",
+    "date": "2022-05-13T18:09:08.859Z",
+    "status": "Pendente",
+    "responsible": "testando3"
+  },
+]
